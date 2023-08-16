0ffd707b7ae5a08cec1a6134993356d1684022ae
yash-vaghasiya <yashvaghasiya602@gmail.com>
2023-08-16 17:29:50 +0530

    crude oparation post method use and put method use mongodb and mysql

--- controller/controller.js
+++ controller/controller.js
@@ -9,6 +9,23 @@ const getMysqlUser =  (req, res) => {
   });
 };
 
+const postmysqlUser = (req, res) => {
+  const { id, name, email, age, mobileNo } = req.body;
+
+  const query = 'INSERT INTO perents (id,name, email, age, mobileNo) VALUES (?, ?, ?, ?, ?)';
+  const values = [id,name, email, age, mobileNo];
+
+  mysqlConnection.query(query, values, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Error inserting into MySQL database" });
+    } else {
+      res.status(201).json({ message: "Data successfully inserted" });
+    }
+  });
+};
+
+
 const getMongodbUser = async (request, response) => {
   try {
     const users = await User.find({});
@@ -17,9 +34,40 @@ const getMongodbUser = async (request, response) => {
     response.status(500).send(error);
   }
 };
+const postMongodbUser = async (req, res) => {
+  try {
+    const { name, email,age,mobileNo } = req.body;
+    const newUser = new User({ name, email, age, mobileNo });
+    const saveuser = await newUser.save();
+    res.status(201).json(saveuser);
+  } catch (error) {
+    res.status(500).json({ error: "Error creating MongoDB user" });
+  }
+};
+
+const edituser = async (req, res) => {
+   try {
+     const { userId } = req.params;
+     console.log("UserID:", userId); // Add this line for debugging
+     const user = await User.findById(userId);
+     console.log("User:", user); // Add this line for debugging
+     
+     if (!user) {
+       return res.status(404).json({ error: "User not found" });
+     }
+     res.json(user);
+   } catch (error) {
+     console.error(error); // Log the error for debugging
+     res.status(500).json({ error: "Error finding user in MongoDB" });
+   }
+};
+
 
 connectToDb;
 module.exports = {
     getMysqlUser,
     getMongodbUser,
+    postMongodbUser,
+    postmysqlUser,
+    edituser,
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,8 @@ const userController = require("../controller/controller.js")
 
 router.get("/mysql",userController.getMysqlUser);
 router.get("/mongodb",userController.getMongodbUser);
+router.post("/post",userController.postMongodbUser);
+router.post("/postmysql",userController.postmysqlUser);
+router.put("/mongodb/:id",userController.edituser);
 
 module.exports = router;
