cb3b0cb5577d72097b3d4249dc2c1815a0d733cb
yash-vaghasiya <yashvaghasiya602@gmail.com>
2023-08-29 16:51:37 +0530

    joi validation set

--- models/userschema.js
+++ models/userschema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {type: String, require: true, trim: true},
-  email: {type: String, require: true, trim: true},
+  email: {type: String, require: true, trim: true,lowercase:true},
   password: {type: String, require: true, trim: true},
   tc: {type: Boolean, require: true}
 });
